143cbd71956931b0a8782e0611449e2d4a74e7a5
Mahedere <mahederetebebe98@gmail.com>
2024-07-10 12:41:09 -0500

    Edit button feature added

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,9 +1,19 @@
-export default function Player({name,symbol}){
-    return<li>
-    <span className="player">
-    <span className="player-name">{name}</span>
-    <span className="player-symbol">{symbol}</span>
-    </span>
-    <button>Edit</button>
-  </li>
+import { useState } from "react"
+export default function Player({ name, symbol }) {
+    const [isEditing, setIsEditing] = useState(false);
+    let playerName=<span className="player-name">{name}</span>
+    function handleEditClick() {
+        setIsEditing(true);
+    }
+    if (isEditing) {
+        playerName=<input type="text" required/>
+    }
+
+    return <li>
+        <span className="player">
+            {playerName}
+            <span className="player-symbol">{symbol}</span>
+        </span>
+        <button onClick={handleEditClick}>Edit</button>
+    </li>
 }
