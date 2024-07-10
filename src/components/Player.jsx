a6504f36b7790444d276f0c6ec0d77a4982ba37b
Mahedere <mahederetebebe98@gmail.com>
2024-07-10 13:04:09 -0500

    save function added

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -3,10 +3,10 @@ export default function Player({ name, symbol }) {
     const [isEditing, setIsEditing] = useState(false);
     let playerName=<span className="player-name">{name}</span>
     function handleEditClick() {
-        setIsEditing(true);
+        setIsEditing(!isEditing);
     }
     if (isEditing) {
-        playerName=<input type="text" required/>
+        playerName=<input type="text" required value={name}/>
     }
 
     return <li>
@@ -14,6 +14,6 @@ export default function Player({ name, symbol }) {
             {playerName}
             <span className="player-symbol">{symbol}</span>
         </span>
-        <button onClick={handleEditClick}>Edit</button>
+        <button onClick={handleEditClick}>{isEditing? 'save':'edit'}</button>
     </li>
 }
