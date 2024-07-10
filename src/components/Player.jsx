03d8d754248976fddc04a90f31497c5733b218e9
Mahedere <mahederetebebe98@gmail.com>
2024-07-10 14:45:38 -0500

    save functionality fixed

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,17 +1,21 @@
 import { useState } from "react"
-export default function Player({ name, symbol }) {
+export default function Player({ initialName, symbol }) {
+    const [playerName,setPlayerName]=useState(initialName)
     const [isEditing, setIsEditing] = useState(false);
-    let playerName=<span className="player-name">{name}</span>
+    let editablePlayerName=<span className="player-name">{playerName}</span>
     function handleEditClick() {
-        setIsEditing(!isEditing);
+        setIsEditing((editing)=>!editing);
+    }
+    function handleChange(event){
+        setPlayerName(event.target.value)
     }
     if (isEditing) {
-        playerName=<input type="text" required value={name}/>
+        editablePlayerName=<input type="text" required  value={playerName} onChange={handleChange}/>
     }
 
     return <li>
         <span className="player">
-            {playerName}
+            {editablePlayerName}
             <span className="player-symbol">{symbol}</span>
         </span>
         <button onClick={handleEditClick}>{isEditing? 'save':'edit'}</button>
