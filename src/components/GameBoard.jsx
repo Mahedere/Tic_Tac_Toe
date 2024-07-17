6bc1e359c05b6043b293afb70ff77e9971de600c
Mahedere <mahederetebebe98@gmail.com>
2024-07-17 12:05:09 -0500

    buttons fixed

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -6,18 +6,18 @@ const initialGameBoard = [
     [null, null, null],
 ];
 
-export default function GameBoard() {
+export default function GameBoard({onSelectSquare,activePlayerSymbol}) {
     const [gameBoard, setGameBoard] = useState(initialGameBoard);
 
-    function handleSelectSquare(rowIndex, colIndex) {
-        setGameBoard((prevGameBoard) => {
-            const updatedBoard = prevGameBoard.map((row, rIndex) =>
-                row.map((cell, cIndex) =>
-                    rIndex === rowIndex && cIndex === colIndex ? 'X' : cell
-                )
-            );
-            return updatedBoard;
-        });
+    function handleSelectSquare(rowIndex,colIndex){
+        setGameBoard((prevGameBoard)=>{
+            const updatedBoard=[...prevGameBoard.map(innerArray=>[...innerArray])]
+            updatedBoard[rowIndex][colIndex]=activePlayerSymbol
+            return updatedBoard
+        }
+        )
+
+        onSelectSquare();
     }
 
     return (
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-export default function Player({ initialName, symbol }) {
+export default function Player({ initialName, symbol,isActive }) {
     const [playerName,setPlayerName]=useState(initialName)
     const [isEditing, setIsEditing] = useState(false);
     let editablePlayerName=<span className="player-name">{playerName}</span>
@@ -13,7 +13,7 @@ export default function Player({ initialName, symbol }) {
         editablePlayerName=<input type="text" required  value={playerName} onChange={handleChange}/>
     }
 
-    return <li>
+    return <li className={isActive?'active':undefined}>
         <span className="player">
             {editablePlayerName}
             <span className="player-symbol">{symbol}</span>
